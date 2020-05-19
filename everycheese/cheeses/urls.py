7b31d39e9a8adf34a180111a0bb75a53d647a62a
Osam Kyemenu-Sarsah <papasarsah@gmail.com>
2020-05-19 10:34:17 +0000

    Implement cheese detail page

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 app_name = "cheeses"
 urlpatterns = [
-    path(
-        '',
-        views.CheeseListView.as_view(),
-        name='list'
-    ),
+    path('',views.CheeseListView.as_view(),name='list'),
+    path('<slug:slug>/',views.CheeseDetailView.as_view(),name='detail'),
 ]
--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -5,4 +5,9 @@ from .models import Cheese
 class CheeseListView(ListView):
 
     model = Cheese
+
+
+class CheeseDetailView(DetailView):
+
+    model = Cheese
     
