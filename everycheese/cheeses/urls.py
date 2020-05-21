cd5ae22f37985c50c55121fea57395c2c658e0d2
Osam Kyemenu-Sarsah <papasarsah@gmail.com>
2020-05-21 19:25:33 +0000

    Add cheese update view, modify template

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.CheeseListView.as_view(),name='list'),
     path('add/', views.CheeseCreateView.as_view(), name='add'),
     path('<slug:slug>/',views.CheeseDetailView.as_view(),name='detail'),
+    path('<slug:slug>/update/', views.CheeseUpdateView.as_view(), name='update'),
     
 ]
--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,4 +1,9 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView
+)
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Cheese
@@ -18,4 +23,10 @@ class CheeseCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+class CheeseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Cheese
+    fields = ['name', 'description', 'firmness', 'country_of_origin']
+    action = "Update"
+    
     
